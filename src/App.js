e5835bbb80327c1b703cb6c4d4793a185c2ecbe0
Sapo Ensaboado <104540418+Cars32@users.noreply.github.com>
2022-12-16 00:18:49 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import {Portifolio_Routers} from './Portifolio/P-Routers'
-//import {Praticando} from './exercicio/pratica/PratiRouters'
-//import {Bootstrap} from './exercicio/exerBootStrap/first-work/bootstrap-Routers.js'
-//import {BootstrapSecond} from './exercicio/exerBootStrap/second-work/BSSW-Routers'
-
 
 
 function App() {
