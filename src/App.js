2e91681bef0dbad5f55e30f052a52eb5a968c478
Sapo Ensaboado <104540418+Cars32@users.noreply.github.com>
2022-12-15 23:53:31 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Portifolio_Routers} from './Portifolio/P-Routers'
+//import {Praticando} from './exercicio/pratica/PratiRouters'
+//import {Bootstrap} from './exercicio/exerBootStrap/first-work/bootstrap-Routers.js'
+//import {BootstrapSecond} from './exercicio/exerBootStrap/second-work/BSSW-Routers'
+
+
+
+function App() {
+  return (
+    <div>
+      <Portifolio_Routers />
+    </div>
+  );
+}
+export default App;
